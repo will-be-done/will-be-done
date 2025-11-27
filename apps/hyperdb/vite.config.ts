a375743ed095577da32cc439f6e33a5789220cb4
Sergey Popov <quolpr@gmail.com>
2025-11-27 23:00:31 +0300

    fix: add test to hyperdb

--- apps/hyperdb/vite.config.ts
+++ apps/hyperdb/vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     browser: {
-      provider: "playwright", // or 'webdriverio'
+      provider: playwright(),
       enabled: true,
       // at least one instance is required
       instances: [{ browser: "chromium" }],
+      headless: true,
     },
   },
 });
