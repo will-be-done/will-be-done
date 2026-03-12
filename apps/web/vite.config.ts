d95ce04e79dd7a1fda5ed20d3d2add47b92db1d2
Sergey Popov <quolpr@gmail.com>
2026-03-12 18:55:08 +0300

    feat: start api sever on random dev port + show logs in TUI

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: `http://localhost:${process.env.VITE_API_PORT || "3000"}`,
         changeOrigin: true,
         secure: false,
         ws: true, // Enable WebSocket proxying
